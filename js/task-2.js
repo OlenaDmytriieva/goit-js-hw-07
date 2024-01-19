f38db955cd9c7817d235bd1266a2d15d382e790a
OlenaDmytriieva <lela.dmitrieva@gmail.com>
2024-01-19 23:45:51 +0200

    task-2

--- js/task-2.js
+++ js/task-2.js
@@ -1,29 +1,57 @@
 const images = [
   {
-    url: 'https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'White and Black Long Fur Cat',
+    url: "https://images.pexels.com/photos/140134/pexels-photo-140134.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "White and Black Long Fur Cat",
   },
   {
-    url: 'https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Orange and White Koi Fish Near Yellow Koi Fish',
+    url: "https://images.pexels.com/photos/213399/pexels-photo-213399.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Orange and White Koi Fish Near Yellow Koi Fish",
   },
   {
-    url: 'https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260',
-    alt: 'Group of Horses Running',
+    url: "https://images.pexels.com/photos/219943/pexels-photo-219943.jpeg?auto=compress&cs=tinysrgb&dpr=2&h=750&w=1260",
+    alt: "Group of Horses Running",
   },
   {
-    url:
-      'https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Elephant Beside on Baby Elephant',
+    url: "https://images.pexels.com/photos/66898/elephant-cub-tsavo-kenya-66898.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    alt: "Elephant Beside on Baby Elephant",
   },
   {
-    url:
-      'https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Blue Geeen and Orange Parrot',
+    url: "https://images.pexels.com/photos/37833/rainbow-lorikeet-parrots-australia-rainbow-37833.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    alt: "Blue Geeen and Orange Parrot",
   },
   {
-    url:
-      'https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2',
-    alt: 'Zebras on Zebra',
+    url: "https://images.pexels.com/photos/247376/pexels-photo-247376.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2",
+    alt: "Zebras on Zebra",
   },
 ];
+
+const myGallery = document.querySelector(".gallery");
+const galleryItems = [];
+images.forEach((element) => {
+  const galleryItem = document.createElement("li");
+  galleryItem.classList.add("gallery-item");
+  const image = document.createElement("img");
+  const url = element.url;
+  image.setAttribute("src", url);
+  const alt = element.alt;
+  image.setAttribute("alt", alt);
+  image.classList.add("image");
+  galleryItem.append(image);
+  galleryItems.push(galleryItem);
+});
+myGallery.append(...galleryItems);
+
+// function gallery() {
+//   const markup = images
+//     .map((image) => {
+//       return `<li class = "gallery-item"><img/></li>`;
+//     })
+//     .join("\n\n");
+
+//   return markup;
+// }
+
+// const markup = gallery();
+// console.log(markup);
+
+// myGallery.insertAdjacentHTML("afterbegin", markup);
