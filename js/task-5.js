4e3fcd861c28ebfd4b737def0c38783900f5cbef
OlenaDmytriieva <lela.dmitrieva@gmail.com>
2024-01-20 13:37:09 +0200

    task-5

--- js/task-5.js
+++ js/task-5.js
@@ -3,3 +3,11 @@ function getRandomHexColor() {
     .toString(16)
     .padStart(6, 0)}`;
 }
+
+const btnElement = document.querySelector(".change-color");
+btnElement.addEventListener("click", (btnEvent) => {
+  const currentColor = document.querySelector(".color");
+  currentColor.textContent = getRandomHexColor();
+  const bodyElement = document.querySelector("body");
+  bodyElement.style.backgroundColor = currentColor.textContent;
+});
