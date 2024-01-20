de5d565c48110ab7c2ae525d776762956b639a26
OlenaDmytriieva <lela.dmitrieva@gmail.com>
2024-01-20 12:52:16 +0200

    task-4

--- js/task-4.js
+++ js/task-4.js
@@ -1,1 +1,15 @@
-
+const formElement = document.querySelector(".login-form");
+formElement.addEventListener("submit", (formEvent) => {
+  formEvent.preventDefault();
+  const userEmail = formElement.elements.email.value.trim();
+  const userPassword = formElement.elements.password.value.trim();
+  const userData = {
+    userEmail,
+    userPassword,
+  };
+  if (userEmail === "" || userPassword === "") {
+    alert("All form fields must be filled in");
+  }
+  console.log(userData);
+  formElement.reset();
+});
